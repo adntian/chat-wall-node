10ed6949db1bc0f26851d4380f5122bc71ba0dad
leesipeng <leesipeng@gmail.com>
2018-11-07 14:01:50 +0800

    fix: 解析appInfo出现? 使用decodeURIComponent

--- websocket.js
+++ websocket.js
@@ -24,7 +24,7 @@ function websocket(server) {
          */
         let index = req.url.indexOf('=');
         if (index != -1) {
-            let encodeStr =(req.url.substr(index + 1)
+            let encodeStr =req.url.substr(index + 1)
             console.log(encodeStr)
             let appInfoStr = decodeURIComponent(encodeStr)
             console.log(appInfoStr)
